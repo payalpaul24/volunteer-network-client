e05772000d99f1dafbc4dfaa205edd8bf179d84e
payalpaul24 <payalpaul2436@gmail.com>
2020-10-06 15:20:12 +0600

    finally hosting complete

---
+++ build/precache-manifest.80e83a4abb36e9f91bfe3bf3e3a03552.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc650e540580a2eeeb28ceb10205b27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b8bc650128599272b0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6f0f0e59d3b2a9617e66",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1b8bc650128599272b0",
+    "url": "/static/js/2.4fd861f7.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.4fd861f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0f0e59d3b2a9617e66",
+    "url": "/static/js/main.3eb53486.chunk.js"
+  },
+  {
+    "revision": "d673e375caa4bfbbc304",
+    "url": "/static/js/runtime-main.6a07803e.js"
+  },
+  {
+    "revision": "36d0242828b09eddb1979d6f55137334",
+    "url": "/static/media/ITHelp.36d02428.png"
+  },
+  {
+    "revision": "a3f3e2df8c5d47bc8e16f23f5f3f408a",
+    "url": "/static/media/animalShelter.a3f3e2df.png"
+  },
+  {
+    "revision": "2e3f62bd0dbea217016481c2137d182a",
+    "url": "/static/media/babySit.2e3f62bd.png"
+  },
+  {
+    "revision": "4888cad44dd51a4d54829ffbf7581514",
+    "url": "/static/media/birdHouse.4888cad4.png"
+  },
+  {
+    "revision": "4d0d6767feb03fcacbfea426393d8cab",
+    "url": "/static/media/childSupport.4d0d6767.png"
+  },
+  {
+    "revision": "bbf7dbaa6cee6265a327823adeb45272",
+    "url": "/static/media/cleanWater.bbf7dbaa.png"
+  },
+  {
+    "revision": "7a0c9a6f635d11afc71969dd27310ce2",
+    "url": "/static/media/clearPark.7a0c9a6f.png"
+  },
+  {
+    "revision": "f3af6ff91d4a417902fea32867fcb40c",
+    "url": "/static/media/clothSwap.f3af6ff9.png"
+  },
+  {
+    "revision": "64270e5fd4c3f1ba6ea26024dbcb15a4",
+    "url": "/static/media/driveSafety.64270e5f.png"
+  },
+  {
+    "revision": "d33d644f1acab104c4b6b501ea3f6245",
+    "url": "/static/media/extraVolunteer.d33d644f.png"
+  },
+  {
+    "revision": "7370fc703a441e47f311ee15ef2cb2f8",
+    "url": "/static/media/foodCharity.7370fc70.png"
+  },
+  {
+    "revision": "2ec0604a9313f3a04b80aa6f71d8ffce",
+    "url": "/static/media/goodEducation.2ec0604a.png"
+  },
+  {
+    "revision": "7a48b1854bd00ab7cec13a7ab0716e2b",
+    "url": "/static/media/home-bg.7a48b185.jpg"
+  },
+  {
+    "revision": "5fe77e0774092b4f60dddeb8af2da7e4",
+    "url": "/static/media/libraryBooks.5fe77e07.png"
+  },
+  {
+    "revision": "b7dd8bbef4a0ef3ceba26c40007aaf82",
+    "url": "/static/media/logo.b7dd8bbe.png"
+  },
+  {
+    "revision": "9a38118eac2beb5467160127eada55cb",
+    "url": "/static/media/musicLessons.9a38118e.png"
+  },
+  {
+    "revision": "33be651294539a17e182f8e57f7843e0",
+    "url": "/static/media/newBooks.33be6512.png"
+  },
+  {
+    "revision": "0169c398ed8a5513bdfde9ff92d5d7ca",
+    "url": "/static/media/refuseShelter.0169c398.png"
+  },
+  {
+    "revision": "5ce3d8f745ff5a49ab6152827855c6a4",
+    "url": "/static/media/riverClean.5ce3d8f7.png"
+  },
+  {
+    "revision": "45d64e055e7d4766ac53204087539a86",
+    "url": "/static/media/schoolSuffiles.45d64e05.png"
+  },
+  {
+    "revision": "3933247e2a899201a8cdc8a4cc0ec7e5",
+    "url": "/static/media/studyGroup.3933247e.png"
+  },
+  {
+    "revision": "72d293e9eb3ef5a32fab277d528747af",
+    "url": "/static/media/stuffedAnimals.72d293e9.png"
+  },
+  {
+    "revision": "c8031e946b9a25a6183110e1c4ef741b",
+    "url": "/static/media/voteRegister.c8031e94.png"
+  }
+]);
